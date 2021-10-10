275623360e8891cca9682d2c98de9eaae5db19a9
Omar Medhat <omarmedhat2004@gmail.com>
2021-10-10 19:51:51 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      green: {
+        light: '#a4b88e',
+        dark: '#58784f',
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
